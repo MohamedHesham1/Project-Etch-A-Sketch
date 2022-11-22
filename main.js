02df8fde0e9cde4ca4105e6e8fc11d0a6bed869b
mohamed <mohamed.hesham.2049@gmail.com>
2022-11-22 04:54:06 +0200

    feat: :sparkles: add 16 * 16 div to gridContainer

--- main.js
+++ main.js
@@ -1,1 +1,17 @@
 import "./style.scss";
+function createGrid() {
+  const container = document.querySelector(".gridContainer");
+  const cells = [];
+  for (let i = 0; i < 256; i++) {
+    cells[i] = document.createElement("div");
+    container.appendChild(cells[i]);
+    cells[i].classList.add("cell");
+  }
+  createTrail(container);
+}
+function createTrail(container) {
+  container.addEventListener("mouseover", (e) => {
+    e.target.style.backgroundColor = "#000000";
+  });
+}
+createGrid();
