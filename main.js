43e5545e5c4c4c3f54063894b1040c3f14526320
mohamed <mohamed.hesham.2049@gmail.com>
2022-11-24 18:06:36 +0200

    style: :lipstick: UI enhancements

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 import "./style.scss";
 const container = document.querySelector(".grid-container");
 const sliderLabel = document.querySelector(".sliderLabel");
+const colorPicker = document.getElementById("color-picker");
 
 function createGrid(value) {
-  let CellsNumber = value * value;
+  const cellsNumber = value * value;
   sliderLabel.innerText = `Grid Size: ${value}*${value}`;
 
-  container.style.cssText = `  
-    grid-template-columns: repeat(${value}, 1fr);
-    grid-template-rows: repeat(${value}, 1fr);
-    `;
-  for (let i = 0; i < CellsNumber; i++) {
+  for (let i = 0; i < cellsNumber; i++) {
     const cell = document.createElement("div");
     container.appendChild(cell);
     cell.classList.add("cell");
   }
+  container.style.cssText = `  
+  grid-template-columns: repeat(${value}, 1fr);
+  grid-template-rows: repeat(${value}, 1fr);
+  `;
   reset();
 }
 
@@ -37,9 +38,6 @@ changeGridSize();
 
 function createTrail() {
   let color;
-  const colorPicker = document.getElementById("color-picker");
-
-  eraseColor(colorPicker);
   generateRandomColor(colorPicker);
   container.addEventListener("mouseover", (e) => {
     color = colorPicker.value;
@@ -48,6 +46,17 @@ function createTrail() {
 }
 createTrail();
 
+function generateRandomColor(colorPicker) {
+  const randomBtn = document.querySelector(".random-color");
+  const maxVal = 0xffffff;
+
+  randomBtn.addEventListener("click", () => {
+    const randomNumber = Math.floor(Math.random() * maxVal);
+    const randomColor = randomNumber.toString(16);
+    colorPicker.value = `#${randomColor}`;
+  });
+}
+
 function eraseColor(colorPicker) {
   const eraser = document.querySelector(".eraser");
 
@@ -55,18 +64,7 @@ function eraseColor(colorPicker) {
     colorPicker.value = "#FFFFFF";
   });
 }
-
-function generateRandomColor(colorPicker) {
-  const randomBtn = document.querySelector(".random-color");
-  const maxVal = 0xffffff;
-
-  randomBtn.addEventListener("click", () => {
-    let randomNumber = Math.random() * maxVal;
-    randomNumber = Math.floor(randomNumber);
-    let randomColor = randomNumber.toString(16);
-    colorPicker.value = `#${randomColor}`;
-  });
-}
+eraseColor(colorPicker);
 
 function reset() {
   const resetBtn = document.querySelector(".reset");
@@ -78,4 +76,3 @@ function reset() {
     });
   });
 }
-reset();
